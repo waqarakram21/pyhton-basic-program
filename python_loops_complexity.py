990299835bd41aa70927a0505b4d3842c5d404d9
Waqar Akram <waqarakrambhutta@gmail.com>
2022-08-18 22:46:43 +0500

    loop and if statment

--- python_loops_complexity.py
+++ python_loops_complexity.py
@@ -2,5 +2,5 @@
 for i in range(10):
     #inner loop
     for j in range(10):
-        print(j ,end=" ")
+        print(0,end=" ")
     print( )
---
+++ python_loops_list.py
@@ -0,0 +1,11 @@
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+
+for i in num_list:
+    print("the number in list is ",i)
+
+for i in num_list:
+    if i >= 45:
+        print("number is greater than 45 so it is", i)
+    else:
+        print("number is less than 45 so it is ",i)
+    
