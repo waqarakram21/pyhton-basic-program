e13d6ec152c84f28b9d6e3f405923f86200da266
Waqar Akram <waqarakrambhutta@gmail.com>
2022-08-24 20:08:49 +0500

    pyhton_sets

---
+++ python_sets.py
@@ -0,0 +1,10 @@
+set_a = {1,2,5,3,4,4,5}
+set_b = {4,5,6,7,8,9}
+
+print(set_a | set_b)#   this is the union of two sets.
+print(set_a & set_b)#   this is the intersection of two sets.
+print(set_a - set_b)#   this is the difference fo two sets.
+print(set_a ^ set_b)#   this is the symmetric intersection of two sets.
+#   these two sets are set_a and set_b.
+
+
