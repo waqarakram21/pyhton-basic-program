c3770b08a4c99f527b6db09062b1f4cc5cd65219
Waqar Akram <waqarakrambhutta@gmail.com>
2022-08-25 10:13:08 +0500

    python_choose_best_Data Structure

---
+++ python_choose_best_DS.py
@@ -0,0 +1,8 @@
+employee_list = [{"id": 12345, "name": "John", "department": "Kitchen"}, {"id": 12458, "name": "Paul", "department": "House Floor"}]
+
+def get_employee(id): 
+    for employee in employee_list:
+        if employee['id'] == id:
+            return employee
+
+print(get_employee(12458))
