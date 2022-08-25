5c6b7d54d5723008c5bf054458feb866a159fb52
Waqar Akram <waqarakrambhutta@gmail.com>
2022-08-25 10:52:59 +0500

    python_choose_best_Data Structure

--- python_choose_best_DS.py
+++ python_choose_best_DS.py
@@ -1,8 +1,25 @@
-employee_list = [{"id": 12345, "name": "John", "department": "Kitchen"}, {"id": 12458, "name": "Paul", "department": "House Floor"}]
+# employee_list = [{"id": 12345, "name": "John", "department": "Kitchen"}, {"id": 12458, "name": "Paul", "department": "House Floor"},{"id":7843, "name":"waqar","department":"computer science"}]
 
-def get_employee(id): 
-    for employee in employee_list:
-        if employee['id'] == id:
-            return employee
+# def get_employee(id): 
+#     for employee in employee_list:
+#         if employee['id'] == id:
+#             return employee
 
-print(get_employee(12458))
+# print(get_employee(7843))
+
+
+# we can also manage this program in much better way because this program have less performance.
+
+employee_list = {
+    12345:
+    {"id": 12345, "name": "John", "department": "Kitchen"},
+    12458:
+    {"id": 12458, "name": "Paul", "department": "House Floor"},
+    7843:
+    {"id":7843, "name":"waqar","department":"computer science"}}
+     
+def get_employee(id):
+   return employee_list[id];
+
+print(get_employee(7843))
+#this is the best way to because it improves the performance of the program. This is because like first solution it does not iterate over the whole list of items,it only match the id and will gives us id.
